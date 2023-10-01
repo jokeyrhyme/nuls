ee59bc017f93f183f53201c6e05c313db66a97c7
Ron Waldon <jokeyrhyme@jokeyrhy.me>
2023-10-01 16:46:40 +1100

    feat: implement default global settings

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-#![deny(clippy::all, unsafe_code)]
+#![deny(clippy::all, clippy::pedantic, unsafe_code)]
 
-use std::borrow::Cow;
+use std::{borrow::Cow, ffi::OsStr, sync::RwLock};
 
-use nu::{convert_position, convert_span, find_line_breaks, IdeComplete, IdeGotoDef, IdeHover};
-use serde_json::Value;
+use error::map_err_to_parse_error;
+use nu::{
+    convert_position, convert_span, find_line_breaks, run_compiler, IdeComplete, IdeGotoDef,
+    IdeHover, IdeSettings,
+};
+
 use tower_lsp::jsonrpc::Result;
+#[allow(clippy::wildcard_imports)]
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+mod error;
 mod nu;
 
 #[derive(Debug)]
 struct Backend {
     client: Client,
+    ide_settings: RwLock<IdeSettings>,
+}
+impl Backend {
+    fn get_document_settings(&self) -> Result<IdeSettings> {
+        Ok(self
+            .ide_settings
+            .read()
+            .map_err(|e| {
+                tower_lsp::jsonrpc::Error::invalid_params(format!("cannot read settings: {e:?}"))
+            })?
+            .clone())
+    }
 }
 
 #[tower_lsp::async_trait]
@@ -34,6 +52,13 @@ impl LanguageServer for Backend {
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
                     TextDocumentSyncKind::NONE,
                 )),
+                workspace: Some(WorkspaceServerCapabilities {
+                    workspace_folders: Some(WorkspaceFoldersServerCapabilities {
+                        supported: Some(true),
+                        change_notifications: Some(OneOf::Left(true)),
+                    }),
+                    ..Default::default()
+                }),
                 ..Default::default()
             },
             server_info: Some(ServerInfo {
@@ -64,37 +89,29 @@ impl LanguageServer for Backend {
             .to_file_path()
             .map_err(|e| {
                 tower_lsp::jsonrpc::Error::invalid_params(format!(
-                    "cannot convert URI to filesystem path: {:?}",
-                    e
+                    "cannot convert URI to filesystem path: {e:?}",
                 ))
             })?;
         let text = tokio::fs::read_to_string(&file_path).await.map_err(|e| {
-            tower_lsp::jsonrpc::Error::invalid_params(format!("cannot read file: {}", e))
+            tower_lsp::jsonrpc::Error::invalid_params(format!("cannot read file: {e:?}"))
         })?;
-        let offset = convert_position(&params.text_document_position.position, &text);
+        let offset = convert_position(params.text_document_position.position, &text);
 
-        // TODO: call nushell Rust code directly instead of via separate process
-        let output = tokio::process::Command::new("nu")
-            .args([
-                "--ide-complete",
-                &format!("{}", offset),
-                &format!("{}", file_path.display()),
-            ])
-            .output()
-            .await
-            .map_err(|e| {
-                let mut err = tower_lsp::jsonrpc::Error::internal_error();
-                err.data = Some(Value::String(format!("{:?}", e)));
-                err.message = Cow::from("`nu --ide-complete` failed");
-                err
-            })?;
+        let ide_settings = self.get_document_settings()?;
+        let output = run_compiler(
+            &text,
+            vec![
+                OsStr::new("--ide-complete"),
+                OsStr::new(&format!("{offset}")),
+            ],
+            ide_settings,
+            params.text_document_position.text_document.uri,
+        )
+        .await?;
 
         let complete: IdeComplete =
-            serde_json::from_slice(output.stdout.as_slice()).map_err(|e| {
-                let mut err = tower_lsp::jsonrpc::Error::parse_error();
-                err.data = Some(Value::String(format!("{:?}", e)));
-                err.message = Cow::from("failed to parse response from `nu --ide-complete`");
-                err
+            serde_json::from_slice(output.stdout.as_bytes()).map_err(|e| {
+                map_err_to_parse_error(e, format!("cannot parse response from {}", output.cmdline))
             })?;
 
         Ok(Some(CompletionResponse::Array(
@@ -117,45 +134,34 @@ impl LanguageServer for Backend {
             .to_file_path()
             .map_err(|e| {
                 tower_lsp::jsonrpc::Error::invalid_params(format!(
-                    "cannot convert URI to filesystem path: {:?}",
-                    e
+                    "cannot convert URI to filesystem path: {e:?}",
                 ))
             })?;
         let text = tokio::fs::read_to_string(&file_path).await.map_err(|e| {
-            tower_lsp::jsonrpc::Error::invalid_params(format!("cannot read file: {}", e))
+            tower_lsp::jsonrpc::Error::invalid_params(format!("cannot read file: {e:?}"))
         })?;
-        let offset = convert_position(&params.text_document_position_params.position, &text);
+        let offset = convert_position(params.text_document_position_params.position, &text);
 
-        // TODO: call nushell Rust code directly instead of via separate process
-        let output = tokio::process::Command::new("nu")
-            .args([
-                "--ide-goto-def",
-                &format!("{}", offset),
-                &format!("{}", file_path.display()),
-            ])
-            .output()
-            .await
-            .map_err(|e| {
-                let mut err = tower_lsp::jsonrpc::Error::internal_error();
-                err.data = Some(Value::String(format!("{:?}", e)));
-                err.message = Cow::from("`nu --ide-goto-def` failed");
-                err
-            })?;
+        let ide_settings = self.get_document_settings()?;
+        let output = run_compiler(
+            &text,
+            vec![
+                OsStr::new("--ide-goto-def"),
+                OsStr::new(&format!("{offset}")),
+            ],
+            ide_settings,
+            params.text_document_position_params.text_document.uri,
+        )
+        .await?;
 
         let goto_def: IdeGotoDef =
-            serde_json::from_slice(output.stdout.as_slice()).map_err(|e| {
-                let mut err = tower_lsp::jsonrpc::Error::parse_error();
-                err.data = Some(Value::String(format!("{:?}", e)));
-                err.message = Cow::from("failed to parse response from `nu --ide-goto-def`");
-                err
+            serde_json::from_slice(output.stdout.as_bytes()).map_err(|e| {
+                map_err_to_parse_error(e, format!("cannot parse response from {}", output.cmdline))
             })?;
 
         let line_breaks = find_line_breaks(&text);
 
-        if matches!(
-            goto_def.file.to_str(),
-            None | Some("") | Some("__prelude__")
-        ) {
+        if matches!(goto_def.file.to_str(), None | Some("" | "__prelude__")) {
             return Ok(None);
         }
 
@@ -170,9 +176,8 @@ impl LanguageServer for Backend {
         }
 
         Ok(Some(GotoDefinitionResponse::Scalar(Location {
-            uri: Url::from_file_path(goto_def.file).map_err(|e| {
+            uri: Url::from_file_path(goto_def.file).map_err(|_| {
                 let mut err = tower_lsp::jsonrpc::Error::parse_error();
-                err.data = Some(Value::String(format!("{:?}", e)));
                 err.message = Cow::from(
                     "failed to parse filesystem path in response from `nu --ide-goto-def`",
                 );
@@ -193,36 +198,25 @@ impl LanguageServer for Backend {
             .to_file_path()
             .map_err(|e| {
                 tower_lsp::jsonrpc::Error::invalid_params(format!(
-                    "cannot convert URI to filesystem path: {:?}",
-                    e
+                    "cannot convert URI to filesystem path: {e:?}",
                 ))
             })?;
         let text = tokio::fs::read_to_string(&file_path).await.map_err(|e| {
-            tower_lsp::jsonrpc::Error::invalid_params(format!("cannot read file: {}", e))
+            tower_lsp::jsonrpc::Error::invalid_params(format!("cannot read file: {e:?}"))
         })?;
-        let offset = convert_position(&params.text_document_position_params.position, &text);
+        let offset = convert_position(params.text_document_position_params.position, &text);
 
-        // TODO: call nushell Rust code directly instead of via separate process
-        let output = tokio::process::Command::new("nu")
-            .args([
-                "--ide-hover",
-                &format!("{}", offset),
-                &format!("{}", file_path.display()),
-            ])
-            .output()
-            .await
-            .map_err(|e| {
-                let mut err = tower_lsp::jsonrpc::Error::internal_error();
-                err.data = Some(Value::String(format!("{:?}", e)));
-                err.message = Cow::from("`nu --ide-hover` failed");
-                err
-            })?;
+        let ide_settings = self.get_document_settings()?;
+        let output = run_compiler(
+            &text,
+            vec![OsStr::new("--ide-hover"), OsStr::new(&format!("{offset}"))],
+            ide_settings,
+            params.text_document_position_params.text_document.uri,
+        )
+        .await?;
 
-        let hover: IdeHover = serde_json::from_slice(output.stdout.as_slice()).map_err(|e| {
-            let mut err = tower_lsp::jsonrpc::Error::parse_error();
-            err.data = Some(Value::String(format!("{:?}", e)));
-            err.message = Cow::from("failed to parse response from `nu --ide-hover`");
-            err
+        let hover: IdeHover = serde_json::from_slice(output.stdout.as_bytes()).map_err(|e| {
+            map_err_to_parse_error(e, format!("cannot parse response from {}", output.cmdline))
         })?;
 
         let line_breaks = find_line_breaks(&text);
@@ -236,6 +230,18 @@ impl LanguageServer for Backend {
             range,
         }))
     }
+
+    async fn did_change_workspace_folders(&self, params: DidChangeWorkspaceFoldersParams) {
+        self.client
+            .log_message(
+                MessageType::INFO,
+                format!(
+                    "workspace folders: added={:?}; removed={:?}",
+                    params.event.added, params.event.removed
+                ),
+            )
+            .await;
+    }
 }
 
 #[tokio::main]
@@ -243,6 +249,9 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        ide_settings: RwLock::new(IdeSettings::default()),
+    });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
--- src/nu.rs
+++ src/nu.rs
@@ -1,14 +1,19 @@
-use std::{cmp::max, path::PathBuf};
+use std::{cmp::max, ffi::OsStr, path::PathBuf, time::Duration};
 
 use serde::Deserialize;
-use tower_lsp::lsp_types::Position;
+use tokio::fs;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::{Position, Url};
+
+use crate::error::{map_err_to_internal_error, map_err_to_parse_error};
 
 #[derive(Deserialize)]
 pub(crate) struct IdeComplete {
     pub completions: Vec<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Default, Deserialize)]
+#[serde(default)]
 pub(crate) struct IdeGotoDef {
     pub end: usize,
     pub file: PathBuf,
@@ -26,8 +31,42 @@ pub(crate) struct IdeHoverSpan {
     pub start: usize,
 }
 
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+pub(crate) struct IdeSettings {
+    pub hints: IdeSettingsHints,
+    pub include_dirs: Vec<PathBuf>,
+    pub max_number_of_problems: u32,
+    pub max_nushell_invocation_time: Duration,
+    pub nushell_executable_path: PathBuf,
+}
+impl Default for IdeSettings {
+    fn default() -> Self {
+        Self {
+            hints: IdeSettingsHints::default(),
+            include_dirs: vec![],
+            max_number_of_problems: 1000,
+            max_nushell_invocation_time: Duration::from_secs(10),
+            nushell_executable_path: PathBuf::from("nu"),
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+pub(crate) struct IdeSettingsHints {
+    pub show_inferred_types: bool,
+}
+impl Default for IdeSettingsHints {
+    fn default() -> Self {
+        Self {
+            show_inferred_types: true,
+        }
+    }
+}
+
 // ported from https://github.com/nushell/vscode-nushell-lang
-pub(crate) fn convert_position(position: &Position, text: &str) -> usize {
+pub(crate) fn convert_position(position: Position, text: &str) -> usize {
     let mut line = 0;
     let mut character = 0;
     let buffer = text.as_bytes();
@@ -78,8 +117,79 @@ pub(crate) fn find_line_breaks(text: &str) -> Vec<usize> {
         .collect()
 }
 
+pub(crate) struct CompilerResponse {
+    pub cmdline: String,
+    pub stdout: String,
+}
+
 // ported from https://github.com/nushell/vscode-nushell-lang
-/// returns the index of the line_break prior to the byte offset
+pub(crate) async fn run_compiler(
+    text: &str,
+    mut flags: Vec<&OsStr>,
+    settings: IdeSettings,
+    uri: Url,
+) -> Result<CompilerResponse> {
+    // TODO: support allowErrors and label options like vscode-nushell-lang?
+
+    let max_number_of_problems = format!("{}", settings.max_number_of_problems);
+    let max_number_of_problems_flag = OsStr::new(&max_number_of_problems);
+    if flags.contains(&OsStr::new("--ide-check")) {
+        flags.push(max_number_of_problems_flag);
+    }
+
+    // record separator character (a character that is unlikely to appear in a path)
+    let record_separator: &OsStr = OsStr::new("\x1e");
+    let mut include_paths: Vec<PathBuf> = vec![];
+    if uri.scheme() == "file" {
+        let file_path = uri.to_file_path().map_err(|e| {
+            tower_lsp::jsonrpc::Error::invalid_params(format!(
+                "cannot convert URI to filesystem path: {e:?}",
+            ))
+        })?;
+        if let Some(p) = file_path.parent() {
+            include_paths.push(p.to_path_buf());
+        }
+    }
+    if !settings.include_dirs.is_empty() {
+        include_paths.extend(settings.include_dirs);
+    }
+    let include_paths: Vec<&OsStr> = include_paths.iter().map(OsStr::new).collect();
+    let include_paths_flag = include_paths.join(record_separator);
+    if !include_paths.is_empty() {
+        flags.push(OsStr::new("--include-path"));
+        flags.push(&include_paths_flag);
+    }
+
+    // vscode-nushell-lang creates this once per single-threaded server process,
+    // but we create this here to ensure the temporary file is used once-per-request
+    let temp_file = mktemp::Temp::new_file().map_err(|e| {
+        map_err_to_internal_error(e, String::from("unable to create temporary file"))
+    })?;
+    fs::write(&temp_file, text).await.map_err(|e| {
+        map_err_to_internal_error(e, String::from("unable to write to temporary file"))
+    })?;
+    flags.push(temp_file.as_os_str());
+
+    let cmdline = format!("nu {flags:?}");
+
+    // TODO: honour max_nushell_invocation_time like vscode-nushell-lang
+    // TODO: call settings.nushell_executable_path
+
+    // TODO: call nushell Rust code directly instead of via separate process,
+    // https://github.com/jokeyrhyme/nuls/issues/7
+    let output = tokio::process::Command::new("nu")
+        .args(flags)
+        .output()
+        .await
+        .map_err(|e| map_err_to_internal_error(e, format!("`{cmdline}` failed")))?;
+    let stdout = String::from_utf8(output.stdout).map_err(|e| {
+        map_err_to_parse_error(e, format!("`{cmdline}` did not return valid UTF-8"))
+    })?;
+    Ok(CompilerResponse { cmdline, stdout })
+}
+
+// ported from https://github.com/nushell/vscode-nushell-lang
+/// returns the index of the line-break prior to the byte offset
 fn lower_bound_binary_search(line_breaks: &[usize], offset: usize) -> Option<usize> {
     if line_breaks.is_empty() {
         return None;
@@ -124,12 +234,12 @@ def main [] {
 
     #[test]
     fn convert_position_ok() {
-        assert_eq!(convert_position(&Position::default(), FIXTURE.trim()), 0);
+        assert_eq!(convert_position(Position::default(), FIXTURE.trim()), 0);
 
         // `ls | ...`
         assert_eq!(
             convert_position(
-                &Position {
+                Position {
                     line: 2,
                     character: 4
                 },
